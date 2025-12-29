532c5c4103ea1a32e8d7404a68e6314e972746dd
Andreas Jansson <ajansson@cloudflare.com>
2025-12-29 22:29:13 +0100

    Test workspace capabilities only

--- lspcmd/lsp/capabilities.py
+++ lspcmd/lsp/capabilities.py
@@ -2,4 +2,18 @@ from typing import Any
 
 
 def get_client_capabilities() -> dict[str, Any]:
-    return {}
+    return {
+        "workspace": {
+            "applyEdit": True,
+            "workspaceEdit": {
+                "documentChanges": True,
+                "resourceOperations": ["create", "rename", "delete"],
+            },
+            "symbol": {
+                "dynamicRegistration": False,
+                "symbolKind": {"valueSet": list(range(1, 27))},
+            },
+            "executeCommand": {"dynamicRegistration": False},
+            "workspaceFolders": True,
+        },
+    }
