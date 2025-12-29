77ba27fea2453891a74ab764630d61c57cd43322
Andreas Jansson <ajansson@cloudflare.com>
2025-12-30 00:24:38 +0100

    fix: import get_log_dir for error messages

--- lspcmd/daemon/session.py
+++ lspcmd/daemon/session.py
@@ -6,6 +6,7 @@ from typing import Any
 
 from ..lsp.client import LSPClient
 from ..lsp.protocol import LanguageServerNotFound, LanguageServerStartupError
+from ..utils.config import get_log_dir
 from ..utils.uri import path_to_uri
 from ..utils.text import get_language_id, read_file_content
 from ..servers.registry import ServerConfig, get_server_for_file, get_server_for_language
