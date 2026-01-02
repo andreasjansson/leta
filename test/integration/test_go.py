1026b7c99e49ab2be2be5f777c09dc9f89dca74f
Andreas Jansson <ajansson@cloudflare.com>
2026-01-02 17:23:14 +0100

    Exclude editable files from grep tests in Go

--- test/integration/test_go.py
+++ test/integration/test_go.py
@@ -155,6 +155,7 @@ utils.go:69 [Function] NewError (func(err error) *Result[T])"""
             "workspace_root": str(workspace),
             "pattern": "Validate",
             "kinds": ["function"],
+            "exclude_patterns": ["editable"],
         })
         output = format_output(response["result"], "plain")
         assert output == """\
@@ -168,6 +169,7 @@ main.go:159 [Function] ValidateUser (func(user *User) error)"""
             "workspace_root": str(workspace),
             "pattern": ".*",
             "kinds": ["function"],
+            "exclude_patterns": ["editable"],
         })
         all_output = format_output(response["result"], "plain")
         assert all_output == """\
