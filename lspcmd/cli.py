1a67fa91e9618fd384d6c2c980f921c8e3fccf97
Andreas Jansson <ajansson@cloudflare.com>
2026-01-02 14:26:48 +0100

    Use Config type in get_workspace_root_for_cwd

--- lspcmd/cli.py
+++ lspcmd/cli.py
@@ -139,7 +139,7 @@ def get_workspace_root_for_path(path: Path, config: Config) -> Path:
     )
 
 
-def get_workspace_root_for_cwd(config: dict[str, Any]) -> Path:
+def get_workspace_root_for_cwd(config: Config) -> Path:
     cwd = Path.cwd().resolve()
 
     workspace_root = get_best_workspace_root(cwd, config)
