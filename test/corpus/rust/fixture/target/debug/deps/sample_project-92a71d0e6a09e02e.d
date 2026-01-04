11b0c8a5092dbeab76f99e0a34c89ceeea946bd1
Andreas Jansson <ajansson@cloudflare.com>
2026-01-05 00:25:09 +0100

    refactor: move language corpus tests to languages/ subdirectory

--- test/corpus/rust/fixture/target/debug/deps/sample_project-92a71d0e6a09e02e.d
+++
@@ -1,8 +0,0 @@
-/Users/ajansson/projects/leta/test/corpus/rust/fixture/target/debug/deps/sample_project-92a71d0e6a09e02e.d: src/main.rs src/user.rs src/storage.rs src/errors.rs
-
-/Users/ajansson/projects/leta/test/corpus/rust/fixture/target/debug/deps/libsample_project-92a71d0e6a09e02e.rmeta: src/main.rs src/user.rs src/storage.rs src/errors.rs
-
-src/main.rs:
-src/user.rs:
-src/storage.rs:
-src/errors.rs:
--- test/corpus/rust/fixture/target/debug/deps/sample_project-a7368e0c64d09ba6.d
+++
@@ -1,8 +0,0 @@
-/Users/ajansson/projects/leta/test/corpus/rust/fixture/target/debug/deps/sample_project-a7368e0c64d09ba6.d: src/main.rs src/user.rs src/storage.rs src/errors.rs
-
-/Users/ajansson/projects/leta/test/corpus/rust/fixture/target/debug/deps/libsample_project-a7368e0c64d09ba6.rmeta: src/main.rs src/user.rs src/storage.rs src/errors.rs
-
-src/main.rs:
-src/user.rs:
-src/storage.rs:
-src/errors.rs:
