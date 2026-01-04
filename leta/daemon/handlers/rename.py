b4dd34247d438de41543880c298f40b8bd206cca
Andreas Jansson <ajansson@cloudflare.com>
2026-01-04 17:47:33 +0100

    try: send only CREATE for modified files, not DELETE+CREATE

--- leta/daemon/handlers/rename.py
+++ leta/daemon/handlers/rename.py
@@ -61,8 +61,6 @@ async def handle_rename(ctx: HandlerContext, params: RPCRenameParams) -> RenameR
     logger.debug(f"Rename: files_modified={files_modified}, renamed_files={renamed_files}")
 
     # Build list of file changes for didChangeWatchedFiles notification
-    # For modified files, send DELETE+CREATE instead of CHANGED to force full reindex
-    # (some servers like ruby-lsp don't properly update their index on CHANGED)
     file_changes: list[tuple[Path, FileChangeType]] = []
     for old_path, new_path in renamed_files:
         file_changes.append((old_path, FileChangeType.Deleted))
@@ -70,8 +68,8 @@ async def handle_rename(ctx: HandlerContext, params: RPCRenameParams) -> RenameR
     for rel_path in files_modified:
         abs_path = workspace_root / rel_path
         if abs_path.exists() and abs_path not in [new for _, new in renamed_files]:
-            # Send DELETE then CREATE to force reindex
-            file_changes.append((abs_path, FileChangeType.Deleted))
+            # Send CREATED to trigger reindex - ruby-lsp will call index_single
+            # since we closed the document before
             file_changes.append((abs_path, FileChangeType.Created))
 
     # Notify LSP about file changes (needed for servers that watch files)
